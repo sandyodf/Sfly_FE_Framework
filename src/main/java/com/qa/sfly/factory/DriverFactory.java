ca5cff5de09e04d2b58726cede27a315e8880901
sandeep vadlapudi <vadlapudisanndeep@gmail.com>
2026-02-01 13:50:37 +0530

    Added  signpage test cases

--- src/main/java/com/qa/sfly/factory/DriverFactory.java
+++ src/main/java/com/qa/sfly/factory/DriverFactory.java
@@ -1,7 +1,39 @@
 package com.qa.sfly.factory;
 
-public class DriverFactory {
+import com.qa.sfly.exceptions.BrowserException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
-    Web
+public class DriverFactory {
+    WebDriver driver;
+
+    public WebDriver initiateBrowser(String browser){
+
+        switch (browser.trim().toLowerCase()){
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            case "Safari":
+                driver = new SafariDriver();
+                break;
+            default:
+                System.out.println("Invalid browser "+browser);
+                throw  new BrowserException("Invalid browser");
+        }
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.get("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
+        return driver;
+
+    }
 
 }
--- src/test/java/com/qa/sfly/base/BaseTest.java
+++ src/test/java/com/qa/sfly/base/BaseTest.java
@@ -1,4 +1,24 @@
 package com.qa.sfly.base;
 
+import com.qa.sfly.factory.DriverFactory;
+import com.qa.sfly.pages.LoginPage;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
 public class BaseTest {
+    WebDriver driver;
+    DriverFactory driverfactory;
+    protected LoginPage lp;
+
+    @BeforeTest
+    public void setup() {
+        driverfactory = new DriverFactory();
+        driver = driverfactory.initiateBrowser("chrome");
+         lp = new LoginPage(driver);
+    }
+    @AfterTest
+    public void tearDown(){
+        driver.close();
+    }
 }
